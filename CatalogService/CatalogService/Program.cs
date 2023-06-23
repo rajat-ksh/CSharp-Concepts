8a0f55a522e01158b05c4560251d9f4f641710c0
rajatkushwah02 <rkushwaha@ebsco.com>
2023-06-23 19:06:33 +0530

    Added config for publising on Azure Service App.

--- CatalogService/CatalogService/Program.cs
+++ CatalogService/CatalogService/Program.cs
@@ -9,18 +9,34 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//.Services.AddDbContext<CatalogServiceAPIDbContext>(options => options.UseInMemoryDatabase("CatalogService"));
-builder.Services.AddDbContext<CatalogServiceAPIDbContext>(options => 
+builder.Services.ConfigureSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "CatalogService",
+        Version = "v1"
+    });
+});
+
+//builder.Services.AddDbContext<CatalogServiceAPIDbContext>(options => options.UseInMemoryDatabase("CatalogService"));
+builder.Services.AddDbContext<CatalogServiceAPIDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CatalogServiceApiConnectionString")));
 
 var app = builder.Build();
 
+app.UseSwagger();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("../swagger/v1/swagger.json", "My API v1");
+    options.RoutePrefix = string.Empty;
+});
+app.UseDeveloperExceptionPage();
 
 app.UseHttpsRedirection();
 
