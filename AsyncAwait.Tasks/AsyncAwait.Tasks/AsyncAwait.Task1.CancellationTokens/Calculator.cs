824672d601427c585d5205dd5b9368927b69abfc
rajat-ksh <akshkushwaha02@gmail.com>
2023-07-06 19:47:03 +0530

    Use of async and await keywords in functions.

--- AsyncAwait.Tasks/AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
+++ AsyncAwait.Tasks/AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -1,21 +1,30 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace AsyncAwait.Task1.CancellationTokens;
-
-internal static class Calculator
+namespace AsyncAwait.Task1.CancellationTokens
 {
-    // todo: change this method to support cancellation token
-    public static long Calculate(int n /*, CancellationToken token*/)
+
+    internal static class Calculator
     {
-        long sum = 0;
-
-        for (var i = 0; i < n; i++)
+        // todo: change this method to support cancellation token
+        public static Task<long> Calculate(int n, CancellationToken token)
         {
-            // i + 1 is to allow 2147483647 (Max(Int32)) 
-            sum = sum + (i + 1);
-            Thread.Sleep(10);
-        }
+            long sum = 0;
 
-        return sum;
+            for (var i = 0; i < n; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    throw new OperationCanceledException();
+                }
+                // i + 1 is to allow 2147483647 (Max(Int32)) 
+                sum = sum + (i + 1);
+                Thread.Sleep(10);
+
+            }
+
+            return Task.FromResult(sum);
+        }
     }
 }
