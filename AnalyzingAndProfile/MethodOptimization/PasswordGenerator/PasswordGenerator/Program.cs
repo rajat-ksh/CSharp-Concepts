adc2b05478573497c1508a2df7b81e25462c30fc
rajat-ksh <akshkushwaha02@gmail.com>
2023-07-10 23:04:47 +0530

    used Buffer.BlockCop insreat of Array.Copy

--- AnalyzingAndProfile/MethodOptimization/PasswordGenerator/PasswordGenerator/Program.cs
+++ AnalyzingAndProfile/MethodOptimization/PasswordGenerator/PasswordGenerator/Program.cs
@@ -5,18 +5,12 @@ Console.WriteLine("Optimise the method GeneratePasswordHashUsingSalt ");
 
 string myPassword = "MyPassword@123";
 
-var specialSalt = new byte[70];
-
-
-for (int i = 0; i < specialSalt.Length; i++)
-{
-    specialSalt[i] = 0x20;
-}
+byte[] specialSalt = new byte[20];
 
 var hashedPassword = GeneratePasswordHashUsingSalt(myPassword, specialSalt);
 Console.WriteLine($"User input password:- {myPassword}.\n Hashedpassword:- {hashedPassword}");
-Console.ReadLine();
 
+Console.ReadLine();
 
 static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
 {
@@ -24,8 +18,8 @@ static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
     var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
     byte[] hash = pbkdf2.GetBytes(20);
     byte[] hashBytes = new byte[36];
-    Array.Copy(salt, 0, hashBytes, 0, 16);
-    Array.Copy(hash, 0, hashBytes, 16, 20);
+    Buffer.BlockCopy(salt, 0, hashBytes, 0, 16);
+    Buffer.BlockCopy(hash, 0, hashBytes, 16, 20);
     var passwordHash = Convert.ToBase64String(hashBytes);
     return passwordHash;
 }
