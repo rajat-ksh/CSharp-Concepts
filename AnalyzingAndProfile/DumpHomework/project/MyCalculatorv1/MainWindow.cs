a58b579fa14d136cb1257ac5c04ec81de0cd355a
rajat-ksh <akshkushwaha02@gmail.com>
2023-07-19 16:12:51 +0530

    Added RegEx for valid input expression.

--- AnalyzingAndProfile/DumpHomework/project/MyCalculatorv1/MainWindow.cs
+++ AnalyzingAndProfile/DumpHomework/project/MyCalculatorv1/MainWindow.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
@@ -7,88 +9,108 @@ namespace MyCalculatorv1;
 
 public partial class MainWindow : Window, IComponentConnector
 {
-	public MainWindow()
-	{
-		InitializeComponent();
-	}
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
 
-	private void Button_Click_1(object sender, RoutedEventArgs e)
-	{
-		Button button = (Button)sender;
-		tb.Text += button.Content.ToString();
-	}
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        Button button = (Button)sender;
+        tb.Text += button.Content.ToString();
+    }
 
-	private void Result_click(object sender, RoutedEventArgs e)
-	{
-		result();
-	}
+    private void Result_click(object sender, RoutedEventArgs e)
+    {
+        if (IsExpressionValid(tb.Text))
+            result();
+        else
+            tb.Text = "Invalid input. Try again!";
 
-	private void result()
-	{
-		int num = 0;
-		if (tb.Text.Contains("+"))
-		{
-			num = tb.Text.IndexOf("+");
-		}
-		else if (tb.Text.Contains("-"))
-		{
-			num = tb.Text.IndexOf("-");
-		}
-		else if (tb.Text.Contains("*"))
-		{
-			num = tb.Text.IndexOf("*");
-		}
-		else if (tb.Text.Contains("/"))
-		{
-			num = tb.Text.IndexOf("/");
-		}
-		string text = tb.Text.Substring(num, 1);
-		double num2 = Convert.ToDouble(tb.Text.Substring(0, num));
-		double num3 = Convert.ToDouble(tb.Text.Substring(num + 1, tb.Text.Length - num - 1));
-		switch (text)
-		{
-		case "+":
-		{
-			TextBox textBox = tb;
-			textBox.Text = textBox.Text + "=" + (num2 + num3);
-			break;
-		}
-		case "-":
-		{
-			TextBox textBox = tb;
-			textBox.Text = textBox.Text + "=" + (num2 - num3);
-			break;
-		}
-		case "*":
-		{
-			TextBox textBox = tb;
-			textBox.Text = textBox.Text + "=" + num2 * num3;
-			break;
-		}
-		default:
-		{
-			TextBox textBox = tb;
-			textBox.Text = textBox.Text + "=" + num2 / num3;
-			break;
-		}
-		}
-	}
+    }
 
-	private void Off_Click_1(object sender, RoutedEventArgs e)
-	{
-		Application.Current.Shutdown();
-	}
+    private bool IsExpressionValid(string expression)
+    {
+        string sanitizedExpression = Regex.Replace(expression, @"\s+", "");
 
-	private void Del_Click(object sender, RoutedEventArgs e)
-	{
-		tb.Text = "";
-	}
+        string pattern = @"^[-+*/()\d]+$";
 
-	private void R_Click(object sender, RoutedEventArgs e)
-	{
-		if (tb.Text.Length > 0)
-		{
-			tb.Text = tb.Text.Substring(0, tb.Text.Length - 1);
-		}
-	}
+        Match match = Regex.Match(sanitizedExpression, pattern);
+
+        if (!match.Success)
+            return false;
+
+        if (Regex.IsMatch(sanitizedExpression, @"[*/+-]{2,}"))
+            return false;
+        return true;
+    }
+
+    private void result()
+    {
+        int num = 0;
+        if (tb.Text.Contains("+"))
+        {
+            num = tb.Text.IndexOf("+");
+        }
+        else if (tb.Text.Contains("-"))
+        {
+            num = tb.Text.IndexOf("-");
+        }
+        else if (tb.Text.Contains("*"))
+        {
+            num = tb.Text.IndexOf("*");
+        }
+        else if (tb.Text.Contains("/"))
+        {
+            num = tb.Text.IndexOf("/");
+        }
+        string text = tb.Text.Substring(num, 1);
+        double num2 = Convert.ToDouble(tb.Text.Substring(0, num));
+        double num3 = Convert.ToDouble(tb.Text.Substring(num + 1, tb.Text.Length - num - 1));
+        switch (text)
+        {
+            case "+":
+                {
+                    TextBox textBox = tb;
+                    textBox.Text = textBox.Text + "=" + (num2 + num3);
+                    break;
+                }
+            case "-":
+                {
+                    TextBox textBox = tb;
+                    textBox.Text = textBox.Text + "=" + (num2 - num3);
+                    break;
+                }
+            case "*":
+                {
+                    TextBox textBox = tb;
+                    textBox.Text = textBox.Text + "=" + num2 * num3;
+                    break;
+                }
+            default:
+                {
+                    TextBox textBox = tb;
+                    textBox.Text = textBox.Text + "=" + num2 / num3;
+                    break;
+                }
+        }
+    }
+
+    private void Off_Click_1(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void Del_Click(object sender, RoutedEventArgs e)
+    {
+        tb.Text = "";
+    }
+
+    private void R_Click(object sender, RoutedEventArgs e)
+    {
+        if (tb.Text.Length > 0)
+        {
+            tb.Text = tb.Text.Substring(0, tb.Text.Length - 1);
+        }
+    }
 }
